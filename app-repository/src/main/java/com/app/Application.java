f8f00eeab491d6909d4e818c3f56220c5869307b
johne <johne@172.19.16.104>
2019-05-20 14:20:17 +0530

    comment

--- app-repository/src/main/java/com/app/Application.java
+++ app-repository/src/main/java/com/app/Application.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootApplication
-//this annotation wouldn't be there for 
+//this annotation wouldn't be there for non-boot packages/sub-projects like this one
+//TODO: need to find other solutions
 public class Application {
 public static void main(String[] args) {
 	SpringApplication.run(Application.class, args);
